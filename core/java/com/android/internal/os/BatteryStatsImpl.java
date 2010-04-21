d28ce5b232d747fbde23c34cda92ef9bb1b9de3e
Koushik Dutta <koushd@gmail.com>
2010-04-21 07:40:29 -0700

    Fix for 50% signal loss reporting bug on Motorola Droid.

--- core/java/com/android/internal/os/BatteryStatsImpl.java
+++ core/java/com/android/internal/os/BatteryStatsImpl.java
@@ -1735,6 +1735,8 @@ public final class BatteryStatsImpl extends BatteryStats {
             addHistoryRecordLocked(SystemClock.elapsedRealtime());
             if (mPhoneSignalStrengthBin >= 0) {
                 mPhoneSignalStrengthsTimer[mPhoneSignalStrengthBin].stopRunningLocked(this);
+            } else {
+                mPhoneSignalStrengthsTimer[SIGNAL_STRENGTH_NONE_OR_UNKNOWN].stopRunningLocked(this);
             }
             mPhoneSignalStrengthBin = bin;
             mPhoneSignalStrengthsTimer[bin].startRunningLocked(this);
